0f00fb1a02fb510320daf75bb00b8100ce88c485
Jacob Chae <jbcden@gmail.com>
2017-01-23 13:46:18 -0500

    Gilded Rose in Ruby

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,3 +1,8 @@
+require './aged_brie'
+require './sulfuras'
+require './backstage_pass'
+require './gilded_rule_factory'
+
 class GildedRose
 
   def initialize(items)
@@ -6,49 +11,9 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      evolving_item = GildedRuleFactory.create(item)
+      evolving_item.update_quality
+      evolving_item.update_sell_in
     end
   end
 end
